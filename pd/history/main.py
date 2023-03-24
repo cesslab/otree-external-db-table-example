ff1356e5164445956ce6221f611694c8ad6ca426
Anwar A. Ruff <anwarruff@gmail.com>
2023-03-24 15:39:19 -0400

    refactor: added sqlalchemy imports for initializing a session and a Base class.

--- pd/history/main.py
+++ pd/history/main.py
@@ -1,3 +1,9 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchem.ext.declarative import declarative_base
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
