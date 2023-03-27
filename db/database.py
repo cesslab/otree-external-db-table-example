40c10729dce940b72e5fed1f589bb420945bab1f
Anwar A. Ruff <anwarruff@gmail.com>
2023-03-27 14:15:15 -0400

    refactor: Add comments to further explain the database initialization process

--- db/database.py
+++ db/database.py
@@ -3,10 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# The base directory should be two levels up from the current file
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# The database url is either the environment variable DATABASE_URL or a local sqlite database
 SQLITE_DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+# Create the database engine
 engine = create_engine(os.getenv("DATABASE_URL", SQLITE_DATABASE_URL))
+# Create a session factory
 Session = sessionmaker(bind=engine)
+# Create a base class for models
 Base = declarative_base()
 
 @staticmethod
